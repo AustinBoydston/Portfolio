d378dc0ff2298972904241cfb45c00175ccacaa8
AustinBoydston <43730698+AustinBoydston@users.noreply.github.com>
2025-02-18 02:33:24 -0600

    reset head~3, added files back. fixed upload file size issue

--- buttonClicks.js
+++ buttonClicks.js
@@ -1,5 +1,28 @@
 const viewWorkButton = document.getElementById("viewWork");
+const viewHomeButton = document.getElementById("homeNav")
+const viewWorkButton2 = document.getElementById("workNav")
+const viewContactButton = document.getElementById("contactNav")
+
+
+const contactPosition = document.getElementById("Contact");
+
+
+const workPosition = document.getElementById("mazeTitle");
 
 viewWorkButton.addEventListener('click', function(){
+    //const targetPosition = workPosition.offsetTop;
+    workPosition.scrollIntoView({behavior: "smooth"});
+});
 
+viewWorkButton2.addEventListener('click', function(){
+    //const targetPosition = workPosition.offsetTop;
+    workPosition.scrollIntoView({behavior: "smooth"});
+});
+viewHomeButton.addEventListener('click', function(){
+    //const targetPosition = workPosition.offsetTop;
+    window.scrollTo({top: 0, behavior: "smooth"});
+});
+viewContactButton.addEventListener('click', function(){
+    //const targetPosition = workPosition.offsetTop;
+    contactPosition.scrollIntoView({behavior: "smooth"});
 });
